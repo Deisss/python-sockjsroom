333fb78feced432af9268251c0705b8419e4519f
Deisss <villette.charles@gmail.com>
2013-10-17 03:36:25 +0200

    switch to version 0.0.2

--- sockjsroom/__init__.py
+++ sockjsroom/__init__.py
@@ -5,7 +5,7 @@
   if available...
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from httpJsonHandler import JsonDefaultHandler
 from socketHandler import SockJSDefaultHandler, SockJSRoomHandler
