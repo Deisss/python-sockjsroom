bbb1991a885da9207597b21243e2b216b1c2aeb9
Deisss <villette.charles@gmail.com>
2013-10-22 00:42:05 +0200

    adding new version release.

--- sockjsroom/__init__.py
+++ sockjsroom/__init__.py
@@ -5,7 +5,7 @@
   if available...
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from httpJsonHandler import JsonDefaultHandler
 from socketHandler import SockJSDefaultHandler, SockJSRoomHandler
